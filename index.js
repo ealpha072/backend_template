4c254f558112c199f971f3ffb8eee8605d45eebd
ealpha072 <ealpha072@gmail.com>
2023-01-08 18:07:29 +0300

    More routes

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-import express from 'express'
-import config from './utils/config.js'
-import cors from 'cors'
-import logger from './utils/logger.js'
-import middlelware from './utils/middleware.js'
+import express from "express";
+import config from "./utils/config.js";
+import cors from "cors";
+import logger from "./utils/logger.js";
+import middlelware from "./utils/middleware.js";
+import appRoute from "./routes/route.js";
 
-logger.info('Attempting connectiong ot localhost')
+logger.info("Attempting connectiong ot localhost");
 
-const app =  express()
-const PORT = config.PORT
+const app = express();
+const PORT = config.PORT;
 
 app.listen(PORT, () => {
-  logger.info(`Server listening on port ${PORT}`)
-})
+  logger.info(`Server listening on port ${PORT}`);
+});
 
-app.use(express.json())
-app.use(middlelware.requestLogger)
+app.use(express.json());
+app.use(middlelware.requestLogger);
+app.use("/app", appRoute);
 
-app.get('/', (req, res) => {
-  res.send('Good morning')
-})
-
-app.use(middlelware.unknownEndpoint)
+app.use(middlelware.unknownEndpoint);
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,13 @@
-import express from 'express'
+import express from "express";
+const appRoute = express.Router();
 
-const appRoute = express.Router()
+appRoute.get("/", (req, res) => {
+  res.send("Good morning Africa");
+});
 
-appRoute.get('/', (req, res) => {
-  res.status(200).json({content:'Server works'})
+appRoute.get("/users", (req, res) => {
+  res.status(200).send('This is a list of all users')
 })
 
-export default appRoute
+
+export default appRoute;
