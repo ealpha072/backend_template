c92c4f5b1bd9cdd3a94f083f7a630717048e5b97
ealpha072 <ealpha072@gmail.com>
2023-01-15 19:32:27 +0300

    Route for login

--- routes/route.js
+++ routes/route.js
@@ -25,12 +25,24 @@ appRoute.post("/register", async (req, res, next) => {
     if (!userExists) {
       const saltRounds = 10;
       const passwordHash = await bcrypt.hash(saltRounds, password);
+      const newUser = new User({ email, username, passwordHash });
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
     } else {
-      res.status(400).json({error: "User already exists"})
+      res.status(400).json({ error: "User already exists" });
     }
   } catch (error) {
     console.log(error.message);
     next(error);
+  }
+});
+
+appRoute.get('/login', async(req, res, next) => {
+  try {
+    
+  } catch (error) {
+    console.log{error.message)
+    next(error)
   }
 });
 
